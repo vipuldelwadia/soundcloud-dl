9982535a5cab04421c3afd84cc7a3bf71e0ec8aa
John K. Luebs <jkluebs@luebsphoto.com>
2013-02-02 21:48:17 -0500

    Have curl follow redirects for page listing URLs
    soundcloud will provides redirects for https URLs in
    certain cases. Make sure curl follow them

--- soundcloud_curl.sh
+++ soundcloud_curl.sh
@@ -8,7 +8,7 @@ if [ -z "$1" ]; then
 	echo "";echo "[i] Usage: `basename $0` http://soundcloud.com/link_with_tracks_on_page";echo "";exit
 fi
 
-pages=`curl -s --user-agent 'Mozilla/5.0' "$1" | tr '"' "\n" | grep "tracks?" | grep "page=" | awk -F= '{print $NF}' | sort -nu | tail -n 1`
+pages=`curl -L -s --user-agent 'Mozilla/5.0' "$1" | tr '"' "\n" | grep "tracks?" | grep "page=" | awk -F= '{print $NF}' | sort -nu | tail -n 1`
 
 if [ -z "$pages" ]; then
 	pages=1
@@ -18,9 +18,9 @@ echo "[i] Found $pages pages of songs!"
 for (( page=1; page <= $pages; page++ ))
 do
 if [ "$pages" = "1" ]; then
-	this=`curl -s --user-agent 'Mozilla/5.0' $1`;
+	this=`curl -L -s --user-agent 'Mozilla/5.0' $1`;
 else
-	this=`curl -s --user-agent 'Mozilla/5.0' $1?page=$page`;
+	this=`curl -L -s --user-agent 'Mozilla/5.0' $1?page=$page`;
 fi
 songs=`echo "$this" | grep 'streamUrl' | tr '"' "\n" | sed 's/\\u0026amp;/\&/' | grep 'http://media.soundcloud.com/stream/' | sed 's/\\\\//'`;
 songcount=`echo "$songs" | wc -l`
